7b5fadfb547c26b766db614508cafeacf4c656d4
ZanhNe <baodoanh1102004@gmail.com>
2024-08-26 07:54:30 +0700

    banking v2.3

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 // BANKIST APP
 
 // Elements
+const body = document.querySelector('body');
 const labelWelcome = document.querySelector('.welcome');
 const labelDate = document.querySelector('.date');
 const labelBalance = document.querySelector('.balance__value');
@@ -99,274 +100,282 @@ const account4 = {
   pin: 4444,
 };
 
+//accounts list
+
 //set appState for initial
 const appState = {
+  accounts: [account1, account2, account3, account4],
+  deposit: `deposit`,
+  withdrawal: `withdrawal`,
   moneyTrans: undefined,
   dateMove: undefined,
   currentAccount: undefined,
   countDown: 100,
   summary: [0, 0, 0],
-};
+  isSort: false,
+  dateCreate: function () {
+    if (!this.date) this.date = new Date();
+    return this.date;
+  },
+  formatterCreate: function () {
+    if (!this.formatter)
+      this.formatter = new Intl.DateTimeFormat('en-GB', {
+        hour: '2-digit',
+        minute: '2-digit',
+      });
+    return this.formatter;
+  },
 
-let date = new Date();
-const formatter = new Intl.DateTimeFormat('en-GB', {
-  hour: '2-digit',
-  minute: '2-digit',
-});
-const formattedTime = formatter.format(date);
+  resetState: function () {
+    this.moneyTrans = undefined;
+    this.dateMove = undefined;
+    this.currentAccount = undefined;
+    this.countDown = 100;
+    this.summary = [0, 0, 0];
+    this.isSort = false;
+  },
+  formattedTime: function () {
+    return this.formatterCreate().format(this.dateCreate());
+  },
+  formatTime: function (seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
 
-const accounts = [account1, account2, account3, account4];
+    // Định dạng số phút và giây để luôn có 2 chữ số
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
 
-//modify display number
-const change = function (number) {
-  let numberStr = String(number.toFixed(2));
-  return numberStr.replace('.', ',').concat(' €');
-};
-
-//convert from seconds to minutes
-function formatTime(seconds) {
-  const minutes = Math.floor(seconds / 60);
-  const remainingSeconds = seconds % 60;
-
-  // Định dạng số phút và giây để luôn có 2 chữ số
-  const formattedMinutes = String(minutes).padStart(2, '0');
-  const formattedSeconds = String(remainingSeconds).padStart(2, '0');
-
-  return `${formattedMinutes}:${formattedSeconds}`;
-}
-
-//set countdown time
-function countDown(callback, seconds) {
-  return callback(seconds);
-}
-
-//add new element DOM
-const addElement = function (value, date, order) {
-  const type = (value < 0 && withdrawal) || deposit;
-  return `
-        <div class="movements__row">
-          <div class="movements__type movements__type--${type}">${
-    order + 1
-  } ${type}</div>
-          <div class="movements__date">${date}</div>
-          <div class="movements__value">${change(value)}</div>
-        </div>
-  `;
-};
-
-//compute username from object's owner
-const computingUsername = fullName => {
-  return fullName
-    .toLowerCase()
-    .split(' ')
-    .map(value => value[0])
-    .join('');
-};
-
-const reset = function () {
-  appState.moneyTrans = undefined;
-  appState.dateMove = undefined;
-  appState.summary = [0, 0, 0];
-  appState.currentAccount = undefined;
-  appState.countDown = 100;
-  containerApp.style.opacity = 0;
-  const allLabelSummary = document.querySelectorAll('.summary__value');
-
-  containerMovements.innerHTML = ``;
-
-  labelWelcome.textContent = `Log in to get started`; //Lời chào mừng
-  for (const el of allLabelSummary) el.textContent = `0000 €`;
-  loginForm.classList.toggle('hidden');
-  labelBalance.textContent = `0000 €`;
-};
-
-//Load time and username
-const init = function () {
-  window.addEventListener('load', () => {
-    const time = setInterval(() => {
-      labelDate.textContent = `${new Date().toLocaleString(
-        'en-GB',
-        formattedTime
-      )}`;
-    }, 1000);
-  });
-  accounts.forEach(account => {
-    account['username'] = computingUsername(account.owner);
-  });
-};
-
-//display
-const displayTransaction = function ({
-  label1,
-  label2,
-  container,
-  value1,
-  value2,
-  totalBalance,
-  dateTrans,
-  size,
-}) {
-  label1.textContent = change(value1);
-  label2.textContent = change(value2);
-  container.insertAdjacentHTML(
-    'afterbegin',
-    addElement(totalBalance, dateTrans, size)
-  );
-};
-
-const setupTrans = function (label1, value1) {
-  const obj = {
-    label1: label1,
-    label2: labelBalance,
-    container: containerMovements,
-    value1: value1,
-    value2: appState.summary[2],
-    totalBalance: appState.moneyTrans.at(-1),
-    dateTrans: appState.dateMove.at(-1),
-    size: appState.moneyTrans.length - 1,
-  };
-  displayTransaction(obj);
-};
-
-const transaction = function (label1, summaryArr, pos, total) {
-  setTimeout(() => {
-    setupTrans(label1, summaryArr[pos]);
-  }, 3000);
-  appState.dateMove.push(
-    `${new Date().toLocaleString('en-GB', formattedTime)}`
-  );
-  appState.moneyTrans.push(total);
-  summaryArr[2] += total;
-  summaryArr[pos] += total;
-};
-
-//Get value from 2 label
-const getValueLabel = function (lbFirst, lbSecond) {
-  return [lbFirst.value, Number(lbSecond.value)];
-};
-
-//Get value from label and delete data on label
-const setupGetValue = function (lbFirst, lbSecond) {
-  const [firstValue, secondValue] = getValueLabel(lbFirst, lbSecond);
-  lbFirst.value = '';
-  lbSecond.value = '';
-  return [firstValue, secondValue];
-};
-
-//setup timeout session
-const timeoutSession = function (interval) {
-  labelTimer.textContent = countDown(formatTime, appState.countDown);
-  if (appState.countDown > 0) {
-    appState.countDown--;
-  } else {
-    clearInterval(interval);
-    setTimeout(reset, 500);
+    return `${formattedMinutes}:${formattedSeconds}`;
+  },
+  modify: function (number) {
+    let numberStr = String(number.toFixed(2));
+    return numberStr.replace('.', ',').concat(' €');
+  },
+  countdown: function (callback, seconds) {
+    return callback(seconds);
+  },
+  addElement: function (value, date, order) {
+    const type = (value < 0 && this.withdrawal) || this.deposit;
+    return `
+          <div class="movements__row">
+            <div class="movements__type movements__type--${type}">${
+      order + 1
+    } ${type}</div>
+            <div class="movements__date">${date}</div>
+            <div class="movements__value">${this.modify(value)}</div>
+          </div>
+    `;
+  },
+  computingUsername: function (fullName) {
+    return fullName
+      .toLowerCase()
+      .split(' ')
+      .map(singleName => singleName[0])
+      .join('');
+  },
+  createUsernames: function () {
+    this.accounts.forEach(acc => {
+      acc['username'] = this.computingUsername(acc.owner);
+    });
+  },
+  reset: function () {
+    this.resetState();
     containerApp.style.opacity = 0;
-  }
-};
-//find account
-const getAccount = function (accounts, username) {
-  return accounts.find(acc => acc.username === username);
-};
+    const allLabelSummary = document.querySelectorAll('.summary__value');
 
-//Login
-btnLogin.addEventListener('click', function (e) {
-  e.preventDefault();
-  const [username, pin] = setupGetValue(inputLoginUsername, inputLoginPin);
-  appState.currentAccount = getAccount(accounts, username);
-  appState.moneyTrans = appState.currentAccount?.movements;
-  appState.dateMove = appState.currentAccount?.movementsDate;
-  const isCorrect = appState.currentAccount?.pin === Number(pin);
-
-  if (isCorrect) {
-    //set timeout session
-    const timeout = setInterval(() => {
-      timeoutSession(timeout);
-    }, 1000);
-
-    //async set display
-    setTimeout(() => {
-      labelSumIn.textContent = change(appState.summary[0]);
-      labelSumOut.textContent = change(appState.summary[1]);
-      labelBalance.textContent = change(appState.summary[2]); //Hiển thị tổng số tiền hiện tại
-      labelWelcome.textContent = `Welcome, ${appState.currentAccount.owner}`; //Lời chào mừng
-      loginForm.classList.toggle('hidden');
-      //Hiển thị trang sau khi login
-      containerApp.style.opacity = 1;
-    }, 0);
-
-    //Xóa element cũ
     containerMovements.innerHTML = ``;
 
-    //calc balance, sum in, sum out
-    appState.summary[2] = appState.moneyTrans.reduce((acc, curr, index) => {
-      curr >= 0 ? (appState.summary[0] += curr) : (appState.summary[1] += curr);
-      containerMovements.insertAdjacentHTML(
-        'afterbegin',
-        addElement(curr, appState.dateMove[index], index)
-      );
-      return acc + curr;
-    }, 0);
-  }
-});
-
-//Transfer
-btnTransfer.addEventListener('click', e => {
-  e.preventDefault();
-  const [transTo, transAmount] = setupGetValue(
-    inputTransferTo,
-    inputTransferAmount
-  );
-  if (appState.summary[2] >= transAmount) {
-    const toPerson = getAccount(accounts, transTo);
-    if (toPerson && toPerson !== appState.currentAccount) {
-      transaction(labelSumOut, appState.summary, 1, -transAmount);
-      toPerson.movementsDate.push(
-        `${new Date().toLocaleString('en-GB', formattedTime)}`
-      );
-      toPerson.movements.push(transAmount);
+    labelWelcome.textContent = `Log in to get started`; //Lời chào mừng
+    for (const el of allLabelSummary) el.textContent = `0000 €`;
+    loginForm.classList.toggle('hidden');
+    labelBalance.textContent = `0000 €`;
+  },
+  setTime: function () {
+    window.addEventListener('load', () => {
+      setInterval(() => {
+        labelDate.textContent = `${new Date().toLocaleString(
+          'en-GB',
+          this.formattedTime()
+        )}`;
+      }, 1000);
+    });
+  },
+  insert: function (mov, index) {
+    console.log(mov, index, this);
+    containerMovements.insertAdjacentHTML(
+      'afterbegin',
+      this.addElement(mov, this.dateMove[index], index)
+    );
+  },
+  displayTransaction: function ({ label1, value1, totalBalance, size }) {
+    label1.textContent = this.modify(value1);
+    labelBalance.textContent = this.modify(this.summary[2]);
+    this.insert(totalBalance, size);
+  },
+  setupTrans: function (label1, value1) {
+    const obj = {
+      label1: label1,
+      value1: value1,
+      totalBalance: this.moneyTrans.at(-1),
+      size: this.moneyTrans.length - 1,
+    };
+    this.displayTransaction(obj);
+  },
+  transaction: function (label1, summaryArr, pos, total) {
+    setTimeout(() => {
+      this.setupTrans(label1, summaryArr[pos]);
+    }, 3000);
+    this.dateMove.push(
+      `${new Date().toLocaleString('en-GB', this.formattedTime())}`
+    );
+    this.moneyTrans.push(total);
+    summaryArr[2] += total;
+    summaryArr[pos] += total;
+  },
+  getValueLabel: function (lbFirst, lbSecond) {
+    return [lbFirst.value, Number(lbSecond.value)];
+  },
+  setupGetValue: function (lbFirst, lbSecond) {
+    const [firstValue, secondValue] = this.getValueLabel(lbFirst, lbSecond);
+    lbFirst.value = lbSecond = ``;
+    return [firstValue, secondValue];
+  },
+  timeoutSession: function (interval) {
+    labelTimer.textContent = this.countdown(this.formatTime, this.countDown);
+    if (appState.countDown > 0) {
+      this.countDown--;
+    } else {
+      clearInterval(interval);
+      setTimeout(this.reset.bind(this), 500);
+      containerApp.style.opacity = 0;
     }
-  }
-});
-
-//Loan
-btnLoan.addEventListener('click', e => {
-  e.preventDefault();
-  const depositAmount = Number(inputLoanAmount.value);
-  if (depositAmount > 0) {
-    transaction(labelSumIn, appState.summary, 0, depositAmount);
-    inputLoanAmount.value = '';
-  }
-});
-
-//Delete account
-btnClose.addEventListener('click', e => {
-  e.preventDefault();
-  const [username, pin] = setupGetValue(inputCloseUsername, inputClosePin);
-  const isCorrect =
-    appState.currentAccount.username === username &&
-    appState.currentAccount.pin === pin;
-
-  if (isCorrect) {
-    setTimeout(reset, 500);
-    containerApp.style.opacity = 0;
-    accounts.splice(accounts.indexOf(appState.currentAccount), 1); //remove this account from original data
-  }
-});
-
-//Sort
-btnSort.addEventListener('click', e => {
-  e.preventDefault();
-  setTimeout(() => {
-    for (let index = copArr.length - 1; index >= 0; index--) {
-      containerMovements.insertAdjacentHTML(
-        'beforeend',
-        addElement(copArr[index], dateMove[index], index)
+  },
+  getAccount: function (accounts, username) {
+    return accounts.find(acc => acc.username === username);
+  },
+  login: function () {
+    btnLogin.addEventListener('click', e => {
+      e.preventDefault();
+      const [username, pin] = this.setupGetValue(
+        inputLoginUsername,
+        inputLoginPin
       );
-    }
-  }, 0);
-  const copArr = appState.moneyTrans.slice().sort((a, b) => a - b);
-  containerMovements.innerHTML = ``;
-});
+      this.currentAccount = this.getAccount(this.accounts, username);
+      this.moneyTrans = this.currentAccount?.movements;
+      this.dateMove = this.currentAccount?.movementsDate;
+      const isCorrect = this.currentAccount?.pin === Number(pin);
 
-init();
+      if (isCorrect) {
+        //set timeout session
+        const timeout = setInterval(() => {
+          this.timeoutSession(timeout);
+        }, 1000);
+
+        //async set display
+        setTimeout(() => {
+          labelSumIn.textContent = this.modify(this.summary[0]);
+          labelSumOut.textContent = this.modify(this.summary[1]);
+          labelBalance.textContent = this.modify(this.summary[2]); //Hiển thị tổng số tiền hiện tại
+          labelWelcome.textContent = `Welcome, ${this.currentAccount.owner}`; //Lời chào mừng
+          loginForm.classList.toggle('hidden');
+          //Hiển thị trang sau khi login
+          containerApp.style.opacity = 1;
+        }, 0);
+
+        //Xóa element cũ
+        containerMovements.innerHTML = ``;
+
+        //calc balance, sum in, sum out
+        this.summary[2] = this.moneyTrans.reduce((acc, curr, index) => {
+          curr >= 0 ? (this.summary[0] += curr) : (this.summary[1] += curr);
+          containerMovements.insertAdjacentHTML(
+            'afterbegin',
+            this.addElement(curr, this.dateMove[index], index)
+          );
+          return acc + curr;
+        }, 0);
+      }
+    });
+  },
+  transfer: function () {
+    btnTransfer.addEventListener('click', e => {
+      e.preventDefault();
+      const [transTo, transAmount] = this.setupGetValue(
+        inputTransferTo,
+        inputTransferAmount
+      );
+      if (transAmount > 0 && this.summary[2] >= transAmount) {
+        const toPerson = this.getAccount(this.accounts, transTo);
+        if (toPerson && toPerson !== this.currentAccount) {
+          this.transaction(labelSumOut, this.summary, 1, -transAmount);
+          toPerson.movementsDate.push(
+            `${new Date().toLocaleString('en-GB', this.formattedTime())}`
+          );
+          toPerson.movements.push(transAmount);
+        }
+      }
+    });
+  },
+  loan: function () {
+    btnLoan.addEventListener('click', e => {
+      e.preventDefault();
+      const depositAmount = Number(inputLoanAmount.value);
+      if (
+        this.currentAccount.movements.some(
+          mov => mov >= 0.1 * depositAmount && depositAmount > 0
+        )
+      ) {
+        this.transaction(labelSumIn, this.summary, 0, depositAmount);
+        inputLoanAmount.value = '';
+      }
+    });
+  },
+  close: function () {
+    btnClose.addEventListener('click', e => {
+      e.preventDefault();
+      const [username, pin] = this.setupGetValue(
+        inputCloseUsername,
+        inputClosePin
+      );
+      const isCorrect =
+        this.currentAccount.username === username &&
+        this.currentAccount.pin === pin;
+
+      if (isCorrect) {
+        setTimeout(this.reset.bind(this), 500);
+        containerApp.style.opacity = 0;
+        this.accounts.splice(
+          this.accounts.findIndex(account => account === this.currentAccount),
+          1
+        ); //remove this account from original data
+      }
+    });
+  },
+  sortMovements: function () {
+    btnSort.addEventListener('click', e => {
+      e.preventDefault();
+      containerMovements.innerHTML = ``;
+      if (!this.isSort) {
+        const copArr = this.moneyTrans.slice().sort((a, b) => a - b);
+        copArr.forEach(this.insert, this);
+      } else {
+        this.moneyTrans.forEach(this.insert, this);
+      }
+      this.isSort = !this.isSort;
+    });
+  },
+  init: function () {
+    this.setTime();
+    this.createUsernames();
+  },
+  start: function () {
+    this.init();
+    this.login();
+    this.transfer();
+    this.loan();
+    this.close();
+    this.sortMovements();
+  },
+};
+
+appState.start();
